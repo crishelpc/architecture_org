0ed7b928ba5b0885653190cc3e51103f23f6255d
crishelpc <202280108@psu.palawan.edu.ph>
2024-01-30 11:54:27 +0800

    modify

---
+++ binary_operation.py
@@ -0,0 +1,17 @@
+
+def division_binary(): 
+    print(f"division")
+
+def multiply_binary(): 
+    print(f"multiply")
+
+def subtract_binary(): 
+    print(f"subtraction")
+
+def add_binary(): 
+    print(f"addition")
+
+def complement(): 
+    print(f"2's complement")
+
+
---
+++ number_conversion.py
@@ -0,0 +1,11 @@
+def binary_to_any(): 
+    print(f"binary to x")
+
+def decimal_to_any(): 
+    print(f"decimal to x")
+
+def octa_to_any(): 
+    print(f"Octa to x")
+
+def hexa_to_any(): 
+    print(f"Hexa to x")
